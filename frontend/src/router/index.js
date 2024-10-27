2a4b8203bd424f99f4eb46ea4aab030f455b4a3e
Raúl Gómez <rgomezca32@alumnes.ub.edu>
2024-10-27 17:50:10 +0100

    Vista del acceso como invitado acabada

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from '../components/login.vue'
 import signup from '../components/signup.vue'
+import guessaccess from '../components/guestaccess.vue'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'guestaccess',
+      component: guessaccess
     },
     {
       path: '/login',
