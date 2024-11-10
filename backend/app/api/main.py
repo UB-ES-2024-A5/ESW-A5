cc5491047cfe0238de350846319af023eaf94a93
damian <dami_1@outlook.com>
2024-11-10 22:13:18 +0100

    Ruta nueva (account)

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 """ Main API routes definition """
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI, HTTPException
 
-from app.api.routes import login, users, utils
+from app.api.routes import login, users, utils, accounts
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
