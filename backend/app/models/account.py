8c19460f83398752726ed373232e61c0794e9bd6
Daniel <danielferrerripoll@gmail.com>
2024-11-11 11:52:19 +0100

    Model Book fet. Criteris d'acceptació revisats. Links son una altre classe i es poden afegir els que sigui necessaris. Cada enllaç es únic.

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -1,9 +1,13 @@
 """ User accounts model class """
 import uuid
 
+from . import User
 from .base import SQLModel
 from sqlmodel import Field, Relationship
 from typing import List
+
+from .book import Book
+
 
 class AccountBase(SQLModel):
     photo: str | None = None
@@ -13,6 +17,7 @@ class Account(AccountBase, table=True):
     id: uuid.UUID = Field(default=None, primary_key=True, foreign_key="user.id")
 
     user: "User" = Relationship(back_populates="account")
+    books: List["Book"] = Relationship(back_populates="account")
 
 class AccountOut(AccountBase):
     id: uuid.UUID
---
+++ backend/app/models/book.py
@@ -0,0 +1,58 @@
+""" Order models """
+import uuid
+
+from sqlmodel import Field, Relationship
+
+from . import User, Account
+from .base import SQLModel
+
+
+# Shared properties
+class BookBase(SQLModel):
+    title: str = Field(max_length=255)
+    author: str = Field(max_length=255)
+    gender_main: str  # Es obligatori un genere pero no dos
+    gender_secondary: str | None = None
+    synopsis: float | None = None
+    publication_year: int | None = None
+    isbn: int | None = Field(unique=True, min_length=10, max_length=13)  #El ISBN es compon de 13 numeros, pero existeixen de 10
+    price: int | None = None
+    img: str | None = None
+
+class Link(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    url: str = Field(unique=True, max_length=255) # cada link tiene ue ser unico
+
+    book_id: uuid.UUID = Field(foreign_key="book.id")
+    book: "Book" = Relationship(back_populates="links")
+
+class LinkUpdate(Link):
+    url: str | None = None
+
+
+# Modelo para la creación de un libro, donde solo incluimos la lista de enlaces
+class BookCreate(BookBase):
+    links: list[LinkUpdate]  # Lista de enlaces para el libro
+
+
+# Database model, database table inferred from class name
+class Book(BookBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    account_id: int = Field(foreign_key="account.id")
+    account: "Account" = Relationship(back_populates="book")
+    links: list[Link] = Relationship(back_populates="book")
+
+
+class BookOut(BookBase):
+    id: int
+
+class BooksOut(SQLModel):
+    data: list[BookOut]
+    count: int
+
+# Són els únics atributs que es poden modificar
+class BookUpdate(BookBase):
+    synopsis: float | None = None
+    price: int | None = None
+    img: str | None = None
+    links: list[LinkUpdate]
