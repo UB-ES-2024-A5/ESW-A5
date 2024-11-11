0ab7150962398b3e777e793516381a47c4ede53b
pausegurab <pausb18@gmail.com>
2024-11-11 18:22:49 +0100

    Tests login and npm update

---
+++ frontend/src/test/e2e/login.spec.js
@@ -0,0 +1,57 @@
+const { test, expect } = require('@playwright/test');
+const sqlite3 = require('sqlite3').verbose();
+
+async function clearUserDatabase() {
+    const path = require('path');
+    const dbPath = path.resolve(__dirname, '../../../../es_db.sqlite');
+    const db = new sqlite3.Database(dbPath);
+    await db.serialize(() => {
+        db.run('DELETE FROM user'); 
+    });
+
+    db.close();
+}
+
+test.describe('Setup - Create User for Login Tests', () => {
+    test('should create a user before login tests', async ({ page }) => {
+      await clearUserDatabase();
+  
+
+      await page.goto('http://localhost:8080/#/');
+      await page.click('text=Sign up as user');
+      await page.fill('input[placeholder="Name"]', 'John');
+      await page.fill('input[placeholder="Surname"]', 'Doe');
+      await page.fill('input[placeholder="Email"]', 'john.doe2@example.com');
+      await page.fill('input[placeholder="Password"]', 'Password!123');
+      await page.fill('input[placeholder="Confirm Password"]', 'Password!123');
+      await page.check('input[type="checkbox"]');
+      await page.click('button.signup-button');
+  
+      const dialogPromise = new Promise((resolve) => {
+        page.on('dialog', async (dialog) => {
+          console.log('Diálogo detectado con mensaje:', dialog.message());
+          expect(dialog.message()).toBe('La cuenta se ha creado correctamente. Por favor inicie sesión.');
+          await dialog.accept();
+          resolve();
+        });
+      });
+      await dialogPromise;
+  
+      await expect(page).toHaveURL('http://localhost:8080/#/login');
+    });
+  });
+  
+  test.describe('Login Page Tests', () => {
+    test('should successfully log in with the created user', async ({ page }) => {
+
+      await page.goto('http://localhost:8080/#/login');  
+      await page.fill('input[placeholder="Email"]', 'john.doe2@example.com');
+      await page.fill('input[placeholder="Password"]', 'Password!123');
+  
+      await page.click('button.login-button');
+
+      await expect(page).toHaveURL(new RegExp('/mainpage_user'));
+
+    });
+
+});
