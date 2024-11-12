1bb0e97ca3fa6591648c644d6eaac3af222d44b9
pausegurab <pausb18@gmail.com>
2024-11-12 17:51:40 +0100

    Testing modified

--- backend/app/tests/crud/test_book.py
+++ backend/app/tests/crud/test_book.py
@@ -2,7 +2,7 @@ from fastapi.encoders import jsonable_encoder
 from sqlmodel import Session
 
 from app import crud
-from app.models import (Book, BookBase, BookCreate, UserCreate, Account)
+from app.models import (Book, BookBase, BookCreate, UserCreate, Account, LinkBase)
 from sqlalchemy.exc import IntegrityError
 from uuid import uuid4
 import pytest
@@ -30,32 +30,37 @@ def create_account(db: Session, create_user):
     return account
 
 
-def test_create_book_all_mandatory_fields(db:Session,create_account):
-    account_id = create_account.id
+def test_create_book_all_mandatory_fields(db:Session,):
+    existing_account = db.exec(Account).first()
 
     title = "Book1"
     author = "Pau"
-    genre1 = "Fantasía"
+    gender_main = "Fantasía"
     synopsis = "lorem ipsum"
     isbn = "7675453127980"
-    account_id_in = account_id
+    account_id_in = existing_account.id
 
-    book_in = BookCreate(title=title,author=author,genre1=genre1,synopsis=synopsis,isbn=isbn,account_id=account_id_in)
+    link1 = LinkBase(url="http://example.com/link1")
+    link2 = LinkBase(url="http://example.com/link2")
+    links = [link1, link2]
+
+    book_in = BookCreate(title=title,author=author,gender_main=gender_main,synopsis=synopsis,isbn=isbn,account_id=account_id_in,links=links)
     book = crud.book.create_book(session=db,book_create=book_in)
 
     assert book.title == title
     assert book.isbn == isbn
+    assert len(book.links) == 2
 
 def test_create_book(db:Session,create_account):
 
-    account_id = create_account.id
+    existing_account = db.exec(Account).first()
 
     title = "Book2"
     author = "Pau"
     genre1 = "Fantasía"
     synopsis = "lorem ipsum"
     isbn = "7675453127981"
-    account_id_in = account_id
+    account_id_in = existing_account.id
     price = 56.3
     publication_year = 2023
 
@@ -68,14 +73,14 @@ def test_create_book(db:Session,create_account):
 
 def test_create_book_isbn_already_in_db(db:Session,create_account):
 
-    account_id = create_account.id
+    existing_account = db.exec(Account).first()
 
     title = "Book6"
     author = "Dani"
     genre1 = "Amor"
     synopsis = "lorem ipsum"
     isbn = "7675453127980"
-    account_id_in = account_id
+    account_id_in = existing_account.id
     price = 30
     publication_year = 1990
 
@@ -85,14 +90,14 @@ def test_create_book_isbn_already_in_db(db:Session,create_account):
         book = crud.book.create_book(session=db,book_create=book_in)
 
 def test_get_book_by_id(db:Session, create_account):
-    account_id = create_account.id
+    existing_account = db.exec(Account).first()
 
     title = "Book5"
     author = "Pau"
     genre1 = "Fantasía"
     synopsis = "lorem ipsum"
     isbn = "7675453127941"
-    account_id_in = account_id
+    account_id_in = existing_account.id
     price = 56.3
     publication_year = 2023
 
