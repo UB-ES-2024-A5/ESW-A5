d61d0ef28d64617f58b13817cfa6245a3254139b
damian <dami_1@outlook.com>
2024-11-13 06:50:50 +0100

    Fix de creacion del datetime del link entre libro y wishlist

--- backend/app/models/wishlist_book_link.py
+++ backend/app/models/wishlist_book_link.py
@@ -8,6 +8,6 @@ from .enums import StateEnum
 class WishlistBookLink(SQLModel, table=True):
     wishlist_id: uuid.UUID | None = Field(default=None, foreign_key="wishlist.id", primary_key=True)
     book_id: uuid.UUID | None = Field(default=None, foreign_key="book.id", primary_key=True)
-    added_date: datetime = Field(default=datetime.now())
+    added_date: datetime = Field(default_factory=datetime.now)
     priority: int | None = Field(default=None)
     state: StateEnum = Field(default=StateEnum.PENDIENTE)
