394bc1ce82ccce460b7d00c7311c5f35dbcc16e8
damian <dami_1@outlook.com>
2024-11-13 06:51:20 +0100

    Endpoints y crud de wishlist (a falta del delete)

--- backend/app/crud/wishlist.py
+++ backend/app/crud/wishlist.py
@@ -5,21 +5,24 @@ from sqlmodel import Session, select
 
 from fastapi import HTTPException
 
-from app.models import WishList, WishListCreate, WishListUpdate
+from app.models import WishList, WishListCreate, WishListUpdate, User
 
-def create_wishlist(session: Session, account: WishListCreate) -> WishList:
+def create_wishlist(session: Session, wishlist: WishListCreate, current_user: User) -> WishList:
     db_obj = WishList(
-
+        name=wishlist.name,
+        icon=wishlist.icon,
+        description=wishlist.description,
+        account_id=current_user.account.id
     )
     session.add(db_obj)
     session.commit()
     session.refresh(db_obj)
     return db_obj
 
-#def update_wishlist(*, session: Session, db_account: Account, account_in: AccountUpdate) -> Any:
- #   account_data = account_in.model_dump(exclude_unset=True)
-  #  db_account.sqlmodel_update(account_data)
-   # session.add(db_account)
-    #session.commit()
-    #session.refresh(db_account)
-    #return db_account
+def update_wishlist(*, session: Session, db_wishlist: WishList, wishlist_in: WishListUpdate) -> Any:
+    wishlist_data = wishlist_in.model_dump(exclude_unset=True)
+    db_wishlist.sqlmodel_update(wishlist_data)
+    session.add(db_wishlist)
+    session.commit()
+    session.refresh(db_wishlist)
+    return db_wishlist
