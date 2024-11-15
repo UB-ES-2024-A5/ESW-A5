e380d1d3aba4c9f66c00362c2d436fc6259da082
pausegurab <pausb18@gmail.com>
2024-11-15 01:37:19 +0100

    View Publisher profile & number of published books

---
+++ frontend/src/services/BookServices.js
@@ -0,0 +1,22 @@
+import http from '../http-common'
+
+class BookService {
+  getBooksByEditorial () {
+    const token = localStorage.getItem('token')
+    return http.get(`/api/v1/books/my_books`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(res => {
+        console.log(res.data)
+        const count = res.data.count
+        return count
+      })
+      .catch(error => {
+        console.error('Error al obtener los libros', error)
+      })
+  }
+}
+
+export default new BookService()
