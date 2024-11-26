45874e6afa9d83aca46dfaf80427d3118a5ad829
Marc Cabrera <marcabrera.mcm@gmail.com>
2024-11-26 23:58:13 +0100

    Links de compra creados

--- frontend/src/components/book.vue
+++ frontend/src/components/book.vue
@@ -24,6 +24,13 @@
           <p><strong>ISBN:</strong> {{ book.isbn || 'ISBN example' }}</p>
           <p><strong>Minimum Price:</strong> {{ book.price || 'Price example' }}</p>
           <p><strong>Synopsis:</strong> {{ book.synopsis || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate.' }}</p>
+          <strong>Links de compra:</strong> <ul>
+          <li v-for="(link,index) in book.list_links" :key="link" class="link-item">
+                <a :href="link" target="_blank" rel="noopener noreferrer">
+                  <button class="link-button">Link {{ index + 1}}</button>
+                </a>
+              </li>
+        </ul>
         </div>
       </div>
     </div>
@@ -64,7 +71,8 @@ export default {
             isbn: res.data.isbn,
             price: res.data.price,
             synopsis: res.data.synopsis,
-            img: res.data.img
+            img: res.data.img,
+            list_links: res.data.list_links || []
           }
 
           this.comments = res.data.comments || []
@@ -252,4 +260,39 @@ export default {
 .star.selected {
   color: goldenrod;
 }
+.link-item{
+  color: #0073e6; /* Un azul profesional */
+  text-decoration: none; /* Elimina el subrayado por defecto */
+  font-weight: bold; /* Da énfasis al texto */
+  padding: 5px; /* Espaciado alrededor del texto */
+  border-radius: 5px; /* Bordes redondeados */
+  transition: all 0.4s ease; /* Suaviza las animaciones */
+  display: inline-block; /* Asegura que respeta el padding */
+  text-align: left;
+
+}
+
+.link-button {
+  background-color: #007bff; /* Fondo azul */
+  color: white; /* Texto blanco */
+  border: none; /* Sin bordes */
+  border-radius: 10px; /* Bordes redondeados */
+  padding: 10px 15px; /* Espaciado interno */
+  font-size: 16px; /* Tamaño de la fuente */
+  font-weight: bold; /* Texto en negrita */
+  cursor: pointer; /* Mostrar el cursor de mano */
+  transition: background-color 0.3s, transform 0.2s; /* Animación suave */
+  text-align: left;
+
+}
+.link-button:hover {
+  background-color: #0056b3; /* Azul más oscuro al pasar el ratón */
+  transform: scale(1.10); /* Efecto de zoom */
+}
+
+.link-button:active {
+  transform: scale(1); /* Reducir zoom al hacer clic */
+  background-color: #004494; /* Fondo aún más oscuro */
+}
+
 </style>
