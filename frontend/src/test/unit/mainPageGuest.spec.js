973c9f3cfe73b5f9371f32546e0c4a8fb007c782
pausegurab <pausb18@gmail.com>
2024-11-28 00:53:43 +0100

    Main Page guest tests done

--- frontend/src/test/unit/mainPageGuest.spec.js
+++ frontend/src/test/unit/mainPageGuest.spec.js
@@ -2,6 +2,11 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import WelcomePage from '../../components/WelcomePage.vue';
 import MainPageGuest from '../../components/MainPageGuest.vue';
+import BookServices from '../../services/BookServices';
+
+jest.mock('../../services/BookServices', () => ({
+  getAllBooks: jest.fn(),
+}));
 
 describe('Navigation from Welcome Page to MainPageGuest', () => {
   let router;
@@ -9,7 +14,10 @@ describe('Navigation from Welcome Page to MainPageGuest', () => {
   localVue.use(VueRouter);
 
   beforeEach(() => {
-    // Configuración de las rutas para Vue 2
+    BookServices.getAllBooks.mockResolvedValue([
+      { id: 1, img: 'book1.png' },
+      { id: 2, img: 'book2.png' },
+    ]);
     const routes = [
       { path: '/', component: WelcomePage },
       { path: '/guest', component: MainPageGuest },
@@ -39,3 +47,52 @@ describe('Navigation from Welcome Page to MainPageGuest', () => {
     expect(router.currentRoute.path).toBe('/mainpage_guest');
   });
 });
+
+describe('Checking MainPage guest', () => {
+  let router;
+  const localVue = createLocalVue();
+  localVue.use(VueRouter);
+
+  beforeEach(() => {
+    const routes = [];
+    router = new VueRouter({ mode: 'history', routes });
+  });
+
+  it('should display a list of books in the carousel', async () => {
+    const wrapper = mount(MainPageGuest, {
+      localVue,
+      router,
+    });
+
+    wrapper.setData({
+      books: [
+        { id: 1, img: 'book1.png' },
+        { id: 2, img: 'book2.png' },
+      ],
+    });
+
+    await wrapper.vm.$nextTick();
+
+    const images = wrapper.findAll('.carousel-image');
+    expect(images.length).toBe(2);
+    expect(images.at(0).attributes('src')).toBe('book1.png');
+  });
+
+  it('should display a login button and not show a profile icon', () => {
+    const wrapper = mount(MainPageGuest, {
+      localVue,
+      router,
+    });
+
+    const loginButton = wrapper.find('.sign-in-btn');
+    expect(loginButton.exists()).toBe(true);
+    expect(loginButton.text()).toBe('Sign in');
+
+    const profileIcon = wrapper.find('.profile-icon');
+    expect(profileIcon.exists()).toBe(false);
+  });
+
+});
+
+
+
