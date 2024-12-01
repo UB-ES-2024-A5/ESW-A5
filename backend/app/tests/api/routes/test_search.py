4c64bcc70568378a25abdd005e215ab7c6a33e1c
damian <dami_1@outlook.com>
2024-12-01 18:35:49 +0100

    User creations for test

---
+++ backend/app/tests/api/routes/test_search.py
@@ -0,0 +1,76 @@
+from unittest.mock import patch
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+from app.main import app
+from app import crud
+from app.core.config import settings
+
+from uuid import uuid4
+import pytest
+from app.tests.utils.utils import random_email, random_lower_string
+
+client = TestClient(app)
+
+@pytest.fixture
+def create_user():
+    user_data = {
+        "email": "usernew232@example.com",
+        "password": "password123",
+        "name": "Editor1",
+        "cif" :"g66666666",
+        "is_editor": True
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+
+@pytest.fixture
+def create_account(create_user):
+    user_out = create_user.json()
+    user_id = user_out['id']
+    account_data = {
+        "id": user_id
+    }
+
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+
+@pytest.fixture
+def create_user_2():
+    user_data = {
+        "email": "usernew231@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname" :"user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+@pytest.fixture
+def create_account_2(create_user_2):
+    account_id = create_user_2.json()["id"]
+    account_data = {
+        "id": account_id
+    }
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+
+@pytest.fixture
+def create_user_3():
+    user_data = {
+        "email": "usernew231@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname" :"user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+@pytest.fixture
+def create_account_3(create_user_3):
+    account_id = create_user_3.json()["id"]
+    account_data = {
+        "id": account_id
+    }
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
