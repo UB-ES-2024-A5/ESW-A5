9100b0da8734d079b93e63281474a4aa1bb428ea
damian <dami_1@outlook.com>
2024-12-02 01:51:48 +0100

    Fixture for tests

--- backend/app/tests/api/routes/test_search.py
+++ backend/app/tests/api/routes/test_search.py
@@ -15,10 +15,10 @@ client = TestClient(app)
 @pytest.fixture
 def create_user():
     user_data = {
-        "email": "usernew232@example.com",
+        "email": "editor23@example.com",
         "password": "password123",
         "name": "Editor1",
-        "cif" :"g66666666",
+        "cif": "g66666666",
         "is_editor": True
     }
     response = client.post("/api/v1/users/", json=user_data)
@@ -38,7 +38,7 @@ def create_account(create_user):
 @pytest.fixture
 def create_user_2():
     user_data = {
-        "email": "usernew231@example.com",
+        "email": "brandon@example.com",
         "password": "password123",
         "name": "new",
         "surname" :"user",
@@ -58,7 +58,7 @@ def create_account_2(create_user_2):
 @pytest.fixture
 def create_user_3():
     user_data = {
-        "email": "usernew231@example.com",
+        "email": "broncano@example.com",
         "password": "password123",
         "name": "new",
         "surname" :"user",
@@ -73,4 +73,144 @@ def create_account_3(create_user_3):
         "id": account_id
     }
     response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+
+@pytest.fixture
+def create_user_4():
+    user_data = {
+        "email": "bruno@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname": "user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+@pytest.fixture
+def create_account_4(create_user_4):
+    account_id = create_user_4.json()["id"]
+    account_data = {
+        "id": account_id
+    }
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+
+@pytest.fixture
+def create_user_5():
+    user_data = {
+        "email": "anabel@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname": "user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+@pytest.fixture
+def create_account_5(create_user_5):
+    account_id = create_user_5.json()["id"]
+    account_data = {
+        "id": account_id
+    }
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+
+@pytest.fixture
+def authenticate():
+    email = "editor23@example.com"
+    password = "password123"
+
+    response = client.post("/api/v1/login/access-token/", data={"username":email, "password":password})
+    return response.json()['access_token']
+
+@pytest.fixture
+def create_book1(authenticate):
+    account = client.get("/api/v1/users/by_email/editor23@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "El camino de los reyes",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466657662",
+        "account_id" : account_id,
+        "price" : 33,
+        "publication_year" : 2015,
+        "links": ["https://www.casadellibro.com/libro-el-camino-de-los-reyes-saga-el-archivo-de-las-tormentas-1/9788466657662/2571296"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book2(authenticate):
+    account = client.get("/api/v1/users/by_email/editor23@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "Palabras Radiantes",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466657549",
+        "account_id" : account_id,
+        "price" : 33,
+        "publication_year" : 2015,
+        "links": ["https://www.casadellibro.com/libro-palabras-radiantes-saga-el-archivo-de-las-tormentas-2/9788466657549/2571295"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book3(authenticate):
+    account = client.get("/api/v1/users/by_email/editor23@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "Juramentada",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788417347000",
+        "account_id" : account_id,
+        "price" : 33,
+        "publication_year" : 2015,
+        "links": ["https://www.casadellibro.com/libro-juramentada-saga-el-archivo-de-las-tormentas-3/9788417347000/6311284"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book4(authenticate):
+    account = client.get("/api/v1/users/by_email/editor23@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "El principe de la niebla",
+        "author" : "Carlos Ruiz Zafon",
+        "gender_main" : "Misterio",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788408163541",
+        "account_id" : account_id,
+        "price" : 33,
+        "publication_year" : 2015,
+        "links": ["https://www.casadellibro.com/libro-el-principe-de-la-niebla/9788408163541/3104118"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
     return response
