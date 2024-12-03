dbd98f53b4e6535b480db7e6e91535899041cfa6
damian <dami_1@outlook.com>
2024-12-03 04:56:44 +0100

    Tests API funcionales

--- backend/app/tests/api/routes/test_accounts.py
+++ backend/app/tests/api/routes/test_accounts.py
@@ -24,20 +24,106 @@ def create_user():
     response = client.post("/api/v1/users/", json=user_data)
     return response
 
-@pytest.fixture
-def create_account(create_user):
-    user_out = create_user.json()
+@pytest.fixture(scope="session")
+def create_super_user():
+    user_data = {
+        "email": "superuser2@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname" :"super",
+        "is_editor": False,
+        "is_superuser" : True
+
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+
+@pytest.fixture(scope="session")
+def create_account_super(create_super_user):
+    user_out = create_super_user.json()
     user_id = user_out["id"]
     account_data = {
         "id": user_id
     }
 
     response = client.post("api/v1/accounts/", json=account_data)
+    if response.status_code == 200:
+        return response  # Devuelve la respuesta original si la creación es exitosa
+    elif response.status_code == 400 and "Account already exists" in response.json().get("detail", ""):
+        # Si ya existe, recuperar la cuenta para reutilizarla
+        return client.get(f"/api/v1/accounts/{user_id}")
+    else:
+        raise Exception(f"Failed to create or retrieve account: {response.json()}")
+
+@pytest.fixture
+def authenticate_super():
+    email = "superuser2@example.com"
+    password = "password123"
+
+    response = client.post("/api/v1/login/access-token/", data={"username":email, "password":password})
+    return response.json()['access_token']
+
+@pytest.fixture(scope="session")
+def create_user2():
+    user_data = {
+        "email": "usernew2@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname" :"user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
     return response
+
+@pytest.fixture(scope="session")
+def create_account2(create_user2):
+    user_out = create_user2.json()
+    user_id = user_out["id"]
+    account_data = {
+        "id": user_id
+    }
+
+    response = client.post("api/v1/accounts/", json=account_data)
+    if response.status_code == 200:
+        return response  # Devuelve la respuesta original si la creación es exitosa
+    elif response.status_code == 400 and "Account already exists" in response.json().get("detail", ""):
+        # Si ya existe, recuperar la cuenta para reutilizarla
+        return client.get(f"/api/v1/accounts/{user_id}")
+    else:
+        raise Exception(f"Failed to create or retrieve account: {response.json()}")
+
+@pytest.fixture(scope="session")
+def create_user3():
+    user_data = {
+        "email": "usernew3@example.com",
+        "password": "password123",
+        "name": "new",
+        "surname" :"user",
+        "is_editor": False
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+
+@pytest.fixture(scope="session")
+def create_account3(create_user3):
+    user_out = create_user3.json()
+    user_id = user_out["id"]
+    account_data = {
+        "id": user_id
+    }
+
+    response = client.post("api/v1/accounts/", json=account_data)
+    if response.status_code == 200:
+        return response  # Devuelve la respuesta original si la creación es exitosa
+    elif response.status_code == 400 and "Account already exists" in response.json().get("detail", ""):
+        # Si ya existe, recuperar la cuenta para reutilizarla
+        return client.get(f"/api/v1/accounts/{user_id}")
+    else:
+        raise Exception(f"Failed to create or retrieve account: {response.json()}")
 
 @pytest.fixture
 def authenticate():
-    email = "usernew@example.com"
+    email = "usernew2@example.com"
     password = "password123"
 
     response = client.post("/api/v1/login/access-token/", data={"username":email, "password":password})
@@ -63,17 +149,17 @@ def test_create_account_with_invalid_user_id():
     response = client.post("api/v1/accounts/", json=account_data)
     assert response.status_code == 400
 
-def test_get_account_me(create_account, authenticate):
+def test_get_account_me(authenticate, create_account2):
     # Obtener la cuenta actual
     headers = {"Authorization": f"Bearer {authenticate}"}
     response = client.get("/api/v1/accounts/me", headers=headers)
     assert response.status_code == 200
-    assert response.json()['id'] == create_account.json()['id']
+    assert response.json()['id'] == create_account2.json()['id']
 
-def test_get_account_by_id(create_account, authenticate):
+def test_get_account_by_id(create_account2, authenticate):
     # Obtener una cuenta específica por ID
     headers = {"Authorization": f"Bearer {authenticate}"}
-    account_id = create_account.json()['id']
+    account_id = create_account2.json()['id']
     response = client.get(f"/api/v1/accounts/{account_id}", headers=headers)
     assert response.status_code == 200
     assert response.json()["id"] == account_id
@@ -86,24 +172,24 @@ def test_get_accounts(authenticate):
     assert "data" in response.json()
     assert "count" in response.json()
 
-def test_update_account_me(create_account, authenticate):
+def test_update_account_me(create_account2, authenticate):
     # Actualizar datos de la cuenta propia
     headers = {"Authorization": f"Bearer {authenticate}"}
-    account_update_data = {"some_field": "new_value"}
+    account_update_data = {"bio": "new_value"}
     response = client.patch("/api/v1/accounts/me", json=account_update_data, headers=headers)
     assert response.status_code == 200
-    assert response.json()["some_field"] == "new_value"
+    assert response.json()["bio"] == "new_value"
 
-def test_update_account_by_id(create_account, authenticate):
+def test_update_account_by_id(create_account2, authenticate_super, create_account_super):
     # Actualizar una cuenta específica por ID
-    headers = {"Authorization": f"Bearer {authenticate}"}
-    account_id = create_account["id"]
-    account_update_data = {"some_field": "updated_value"}
+    headers = {"Authorization": f"Bearer {authenticate_super}"}
+    account_id = create_account2.json()['id']
+    account_update_data = {"bio": "updated_value"}
     response = client.patch(f"/api/v1/accounts/{account_id}", json=account_update_data, headers=headers)
     assert response.status_code == 200
-    assert response.json()["some_field"] == "updated_value"
+    assert response.json()["bio"] == "updated_value"
 
-def test_delete_account_me(create_account, authenticate):
+def test_delete_account_me(create_account2, authenticate):
     # Eliminar la cuenta actual
     headers = {"Authorization": f"Bearer {authenticate}"}
     response = client.delete("/api/v1/accounts/me", headers=headers)
@@ -111,18 +197,17 @@ def test_delete_account_me(create_account, authenticate):
     assert response.json()["message"] == "Account deleted successfully"
 
 
-def test_delete_account_by_id(create_account, authenticate):
+def test_delete_account_by_id(create_account3, authenticate_super, create_account_super):
     # Eliminar una cuenta específica por ID
-    headers = {"Authorization": f"Bearer {authenticate}"}
-    account_id = create_account["id"]
+    headers = {"Authorization": f"Bearer {authenticate_super}"}
+    account_id = create_account3.json()['id']
     response = client.delete(f"/api/v1/accounts/{account_id}", headers=headers)
     assert response.status_code == 200
     assert response.json()["message"] == "Account deleted successfully"
 
-def test_get_account_not_found(authenticate):
+def test_get_account_not_found():
     # Obtener una cuenta inexistente
-    headers = {"Authorization": f"Bearer {authenticate}"}
     non_existing_account_id = uuid4()
-    response = client.get(f"/api/v1/accounts/{non_existing_account_id}", headers=headers)
+    response = client.get(f"/api/v1/accounts/{non_existing_account_id}")
     assert response.status_code == 404
     assert response.json()["detail"] == "Account not found"
