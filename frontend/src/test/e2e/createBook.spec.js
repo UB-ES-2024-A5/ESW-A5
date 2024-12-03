161f9ed42a72b1807769f66196ec91af3a726bdd
pausegurab <pausb18@gmail.com>
2024-12-03 12:48:53 +0100

    Test end2end for creating a book and fix test for signup

--- frontend/src/test/e2e/createBook.spec.js
+++ frontend/src/test/e2e/createBook.spec.js
@@ -38,11 +38,12 @@ async function clearUserDatabase() {
     }
   }
 
+  let bookId;
   test.describe('Set up publisher account', () => {
     test('Create a user and a account', async () => {
       await clearUserDatabase();
       const apiUrl = 'http://localhost:8000/api/v1';
-  
+
       const userData = {
         email: 'testuser@example.com',
         name: 'John Doe',
@@ -50,32 +51,77 @@ async function clearUserDatabase() {
         is_editor: true,
         cif: 'G12345678',
       };
-  
+
       let response = await fetch(`${apiUrl}/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData),
       });
-  
+
       expect(response.status).toBe(200);
       const userDataResponse = await response.json();
-  
+
       const accountData = {
         id: userDataResponse.id,
       };
-  
+
       response = await fetch(`${apiUrl}/accounts`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(accountData),
       });
-  
+
       expect(response.status).toBe(200);
       const accountDataResponse = await response.json();
-    });
-  });
 
+      const loginData = {
+        username: userData.email,
+        password: userData.password,
+      };
+
+      response = await fetch(`${apiUrl}/login/access-token`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(loginData),
+      });
+
+      expect(response.status).toBe(200);
+      const loginResponse = await response.json();
+      const accessToken = loginResponse.access_token;
+
+      const bookData = {
+        title: 'The Great Book',
+        author: 'Jane Author',
+        gender_main: 'Fiction',
+        gender_secondary: 'Drama',
+        synopsis: 'A gripping novel about...',
+        publication_year: 2024,
+        isbn: '9781234567890',
+        price: 19.99,
+        img: 'path/to/book/image.jpg',
+        links: ['http://example.com'],
+      };
+
+      response = await fetch(`${apiUrl}/books`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      expect(response.status).toBe(200);
+      const bookDataResponse = await response.json();
+
+      expect(bookDataResponse).toHaveProperty('id');
+      bookId = bookDataResponse.id;
+      expect(bookDataResponse.title).toBe(bookData.title);
+  });
+});
+  
   test.describe('E2E Book Page', () => {
+    
     test('Should redirect to login if not authenticated and trying to access /mainPage_user', async ({ page }) => {
     await page.goto('http://localhost:8080')
     await page.goto('http://localhost:8080/mainPage_publisher');
@@ -83,4 +129,93 @@ async function clearUserDatabase() {
     await expect(page).toHaveURL('http://localhost:8080/login');
 
     });
+  
+    test('Should open the form on click + button', async ({page}) => {
+      await page.goto('http://localhost:8080/login');
+
+      await page.fill('input[placeholder="Email"]', 'testuser@example.com');
+      await page.fill('input[placeholder="Password"]', 'testpassword');
+      await page.click('button.login-button');
+      await expect(page)-toHaveURL('http://localhost:8080/mainPage_publisher');
+
+      const addButton = page.locator('.add-button');
+      await expect(addButton).toBeVisible();
+
+      await addButton.click();
+
+      await expect(page).toHaveURL('http://localhost:8080/create_publication');
+
+    });
+
+    test('Should create correctly a book', async ({page}) => {
+
+      await page.goto('http://localhost:8080/login');
+
+      await page.fill('input[placeholder="Email"]', 'testuser@example.com');
+      await page.fill('input[placeholder="Password"]', 'testpassword');
+      await page.click('button.login-button');
+      await expect(page)-toHaveURL('http://localhost:8080/mainPage_publisher');
+
+      const addButton = page.locator('.add-button');
+      await expect(addButton).toBeVisible();
+
+      await addButton.click();
+
+      await expect(page).toHaveURL('http://localhost:8080/create_publication');
+      await page.fill('input[placeholder="Title"]', 'Valid Title');
+      await page.fill('input[placeholder="Author"]', 'Valid Author');
+      await page.fill('input[placeholder="Editorial"]', 'Editorial');
+      await page.fill('input[placeholder="Year of Publication"]', '2023');
+      await page.fill('input[placeholder="ISBN"]', '1234567890123');
+      await page.fill('textarea[placeholder="Synopsis"]', 'A test synopsis.');
+      await page.fill('input[placeholder="Price"]', '19.99');
+      await page.check('input[value="Fiction"]');
+      const imagePath = 'src/test/assets/prueba.jpg';
+      await page.setInputFiles('input[type="file"]', imagePath);
+      await page.click('button[type="submit"]');
+
+      const swal = page.locator('.swal2-container');
+      await expect(swal).toBeVisible();
+
+      const swalTitle = swal.locator('.swal2-title');
+      const swalText = swal.locator('.swal2-html-container');
+      await expect(swalTitle).toHaveText('Success');
+
+    });
+
+    test('Should fail creating a book with repeated isbn', async ({page}) => {
+      await page.goto('http://localhost:8080/login');
+
+      await page.fill('input[placeholder="Email"]', 'testuser@example.com');
+      await page.fill('input[placeholder="Password"]', 'testpassword');
+      await page.click('button.login-button');
+      await expect(page)-toHaveURL('http://localhost:8080/mainPage_publisher');
+
+      const addButton = page.locator('.add-button');
+      await expect(addButton).toBeVisible();
+
+      await addButton.click();
+
+      await expect(page).toHaveURL('http://localhost:8080/create_publication');
+      await page.fill('input[placeholder="Title"]', 'Valid Title');
+      await page.fill('input[placeholder="Author"]', 'Valid Author');
+      await page.fill('input[placeholder="Editorial"]', 'Editorial');
+      await page.fill('input[placeholder="Year of Publication"]', '2023');
+      await page.fill('input[placeholder="ISBN"]', '9781234567890');
+      await page.fill('textarea[placeholder="Synopsis"]', 'A test synopsis.');
+      await page.fill('input[placeholder="Price"]', '19.99');
+      await page.check('input[value="Fiction"]');
+      const imagePath = 'src/test/assets/prueba.jpg';
+      await page.setInputFiles('input[type="file"]', imagePath);
+      await page.click('button[type="submit"]');
+
+      const swal = page.locator('.swal2-container');
+      await expect(swal).toBeVisible();
+
+      const swalTitle = swal.locator('.swal2-title');
+      const swalText = swal.locator('.swal2-html-container');
+      await expect(swalTitle).toHaveText('Error!')
+
+    });
+
 });
