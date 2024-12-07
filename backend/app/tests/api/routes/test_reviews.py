9150a5cbe2d7a1dc1b8f36b524625bdfe35492fb
damian <dami_1@outlook.com>
2024-12-07 21:27:58 +0100

    Fixtures for api #2

--- backend/app/tests/api/routes/test_reviews.py
+++ backend/app/tests/api/routes/test_reviews.py
@@ -72,6 +72,208 @@ def authenticate2():
     response = client.post("/api/v1/login/access-token/", data={"username": email, "password": password})
     return response.json()['access_token']
 
+@pytest.fixture
+def create_user_editor():
+    user_data = {
+        "email": "editorreview1@example.com",
+        "password": "password123",
+        "name": "Editor1",
+        "cif" :"g66666666",
+        "is_editor": True
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+
+@pytest.fixture
+def create_account_editor(create_user_editor):
+    user_out = create_user_editor.json()
+    user_id = user_out['id']
+    account_data = {
+        "id": user_id
+    }
+
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+@pytest.fixture
+def authenticate_editor():
+    email = "editorreview1@example.com"
+    password = "password123"
+
+    response = client.post("/api/v1/login/access-token/", data={"username":email, "password":password})
+    return response.json()['access_token']
+
+@pytest.fixture
+def create_user_editor2():
+    user_data = {
+        "email": "editorreview2@example.com",
+        "password": "password123",
+        "name": "Editor2",
+        "cif" :"g66666666",
+        "is_editor": True
+    }
+    response = client.post("/api/v1/users/", json=user_data)
+    return response
+
+@pytest.fixture
+def create_account_editor2(create_user_editor2):
+    user_out = create_user_editor2.json()
+    user_id = user_out['id']
+    account_data = {
+        "id": user_id
+    }
+
+    response = client.post("/api/v1/accounts/", json=account_data)
+    return response
+@pytest.fixture
+def authenticate_editor2():
+    email = "editorreview2@example.com"
+    password = "password123"
+
+    response = client.post("/api/v1/login/access-token/", data={"username":email, "password":password})
+    return response.json()['access_token']
+
+@pytest.fixture
+def create_book1(authenticate_editor):
+    account = client.get("/api/v1/users/by_email/editorreview1@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "El imperio final",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788498726138",
+        "account_id" : account_id,
+        "price" : 12.30,
+        "publication_year" : 2020,
+        "links": ["https://www.casadellibro.com/libro-el-imperio-final-saga-nacidos-de-la-bruma-1/9788498726138/1956329"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book2(authenticate_editor):
+    account = client.get("/api/v1/users/by_email/editorreview1@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "El pozo de la ascension",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788418037276",
+        "account_id" : account_id,
+        "price" : 28.40,
+        "publication_year" : 2021,
+        "links": ["https://www.casadellibro.com/libro-el-pozo-de-la-ascension-nacidos-de-la-bruma-mistborn-edicion-ilustrada-2/9788418037276/12382307"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book3(authenticate_editor):
+    account = client.get("/api/v1/users/by_email/editorreview1@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "El heroe de las eras",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466658911",
+        "account_id" : account_id,
+        "price" : 23.65,
+        "publication_year" : 2016,
+        "links": ["https://www.casadellibro.com/libro-el-heroe-de-las-eras-saga-nacidos-de-la-bruma-3/9788466658911/3082061"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book4(authenticate_editor2):
+    account = client.get("/api/v1/users/by_email/editorreview2@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "Aleacion de ley",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466658928",
+        "account_id" : account_id,
+        "price" : 23.65,
+        "publication_year" : 2016,
+        "links": ["https://www.casadellibro.com/libro-aleacion-de-ley-saga-nacidos-de-la-bruma-4/9788466658928/3082062"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor2}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book5(authenticate_editor2):
+    account = client.get("/api/v1/users/by_email/editorreview2@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "Sombras de identidad",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466658850",
+        "account_id" : account_id,
+        "price" : 23.65,
+        "publication_year" : 2016,
+        "links": ["https://www.casadellibro.com/libro-sombras-de-identidad-saga-nacidos-de-la-bruma-5/9788466658850/3082063"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor2}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
+@pytest.fixture
+def create_book6(authenticate_editor2):
+    account = client.get("/api/v1/users/by_email/editorreview2@example.com")
+    account_id = account.json()['id']
+    book_data = {
+        "title" : "Brazales de duelo",
+        "author" : "Brandon Sanderson",
+        "gender_main" : "Fantasia",
+        "synopsis" : "lorem ipsum",
+        "isbn" : "9788466659628",
+        "account_id" : account_id,
+        "price" : 23.65,
+        "publication_year" : 2016,
+        "links": ["https://www.casadellibro.com/libro-brazales-de-duelo-saga-nacidos-de-la-bruma-6/9788466659628/5420146"]
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate_editor2}"
+    }
+
+    response = client.post("/api/v1/books/", json=book_data, headers=headers)
+    return response
+
 # Variable para almacenar datos entre pruebas
 created_account_data = {}
 
+def test_create_review_point_book(create_account, create_account2, create_account_editor, create_account_editor2,
+                                  create_book, create_book2, create_book3, create_book4, create_book5, create_book6,
+                                  authenticate):
+    pass
