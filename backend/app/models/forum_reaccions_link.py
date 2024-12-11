7568b2f04dd5f6619c28ce2ebd86f304b060d3cb
Daniel <danielferrerripoll@gmail.com>
2024-12-11 16:55:40 +0100

    US10: CRUD y END Forum
    Get: Hay Get de todo: get de los post con like y dislike, tanto currentUser como por account_id. Get de mis post y por account_id (tanto post como responses aparece). Get post by id, tipico y facil. Y el mas importante Get post de mis followings, es decir los post de las accounts que siguo (solo salen los que son Post principal y no responses que hacen)
    
    PostForum: El post normal sin nada, facil de entender. Cuando eliminas un post se elimina todo el arbol que genera, tanto de responses como de reactions.
    
    PostResponse: El post con post_parent_id, passas el id del post que estas respondiendo
    
    ReactionForum: Cuando se crea passas el id del post que haces la reaccion. Solo puede habaer uno por account-post. Si lo quieres cambiar hay el patch. Y para eliminar el delete (a todos pasas el post_id a reaccionar). La idea es que si like y dislike no estan marcados cree, si hay un marcada y clicas a otro patch y si clickas el seleccionado que se elimine.

--- backend/app/models/forum_reaccions_link.py
+++ backend/app/models/forum_reaccions_link.py
@@ -4,9 +4,15 @@ from datetime import datetime, timezone
 from sqlmodel import SQLModel, Field, Relationship
 from typing import List, Optional
 
+from app.models import Account
+
 
 # Relacion muchos a muchos enre forum i account: Esto es necesario porque una cuenta puede dar "like" o "dislike"
 # a múltiples foros, y cada foro puede recibir "likes" o "dislikes" de múltiples cuentas.
+class ForumReactionBase(SQLModel):
+    type: bool  # "like" o "dislike"
+
+
 class ForumReaction(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     forum_id: uuid.UUID = Field(foreign_key="forum.id")  # Relación con Forum
@@ -16,3 +22,13 @@ class ForumReaction(SQLModel, table=True):
     account: "Account" = Relationship(back_populates="reactions")
 
     type: bool = Field(default=True, max_length=10)  # "like" = True o "dislike" = False
+
+
+class ForumReactionUpdate(ForumReactionBase):
+    pass
+
+
+class ForumReactionOut(ForumReactionBase):
+    id: uuid.UUID
+    forum_id: uuid.UUID
+    account_id: uuid.UUID
