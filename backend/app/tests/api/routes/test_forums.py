806803a4627da5b95af9b7867aba62e88954a60e
damian <dami_1@outlook.com>
2024-12-13 20:24:28 +0100

    Added file

---
+++ backend/app/tests/api/routes/test_forums.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+from app.main import app
+from app import crud
+from app.core.config import settings
+
+from uuid import uuid4
+import pytest
+from app.tests.utils.utils import random_email, random_lower_string
+
+client = TestClient(app)
+
+# Variable para almacenar datos entre pruebas
+created_account_data_forum = {}
