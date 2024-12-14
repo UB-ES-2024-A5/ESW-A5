32b8456772521c53f705adb19947abdb0f70b654
damian <dami_1@outlook.com>
2024-12-14 02:16:13 +0100

    Complete api tests

--- backend/app/tests/api/routes/test_forums.py
+++ backend/app/tests/api/routes/test_forums.py
@@ -269,3 +269,273 @@ def test_get_responses_by_post():
     assert results["count"] == 1
 
 
+def test_create_reaction_non_existent_post(authenticate):
+    post_id = uuid4()
+
+    reaction_data = {
+        "type": True
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post(f"/api/v1/forums/create_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Post Forum does not exist"
+
+
+def test_create_reaction(authenticate):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    reaction_data = {
+        "type": True
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post(f"/api/v1/forums/create_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["type"] == reaction_data["type"]
+
+
+def test_create_reaction_duplicate(authenticate):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    reaction_data = {
+        "type": True
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.post(f"/api/v1/forums/create_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 400
+    results = response.json()
+    assert results["detail"] == "Reaction already exists."
+
+
+
+def test_update_reaction_post_not_found(authenticate):
+    post_id = uuid4()
+
+    reaction_data = {
+        "type": False
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.patch(f"/api/v1/forums/update_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Post Forum does not exist"
+
+
+def test_update_reaction_not_found(authenticate2):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    reaction_data = {
+        "type": False
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate2}"
+    }
+
+    response = client.patch(f"/api/v1/forums/update_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 400
+    results = response.json()
+    assert results["detail"] == "Reaction does not exists."
+
+
+def test_update_reaction(authenticate):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    reaction_data = {
+        "type": False
+    }
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.patch(f"/api/v1/forums/update_reaction/{post_id}", json=reaction_data, headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["type"] == reaction_data["type"]
+
+
+def test_get_posts_my_like(authenticate):
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.get(f"/api/v1/forums/posts_me_like/", headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["count"] == 0
+
+
+def test_get_posts_my_dislike(authenticate):
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.get(f"/api/v1/forums/posts_me_dislike/", headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["count"] == 1
+
+
+def test_get_posts_account_like():
+    account_id = created_account_data_forum["account1"]["id"]
+
+    response = client.get(f"/api/v1/forums/posts_like/{account_id}/")
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["count"] == 0
+
+
+def test_get_posts_account_like_not_found():
+    account_id = uuid4()
+
+    response = client.get(f"/api/v1/forums/posts_like/{account_id}/")
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Account does not exist"
+
+
+def test_get_posts_account_dislike_not_found():
+    account_id = uuid4()
+
+    response = client.get(f"/api/v1/forums/posts_dislike/{account_id}/")
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Account does not exist"
+
+
+def test_get_posts_account_dislike():
+    account_id = created_account_data_forum["account1"]["id"]
+
+    response = client.get(f"/api/v1/forums/posts_dislike/{account_id}/")
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["count"] == 1
+
+
+def test_delete_reaction_not_found_post(authenticate):
+    post_id = uuid4()
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_reaction/{post_id}", headers=headers)
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Post Forum does not exist"
+
+
+def test_delete_reaction_not_belong_user(authenticate):
+    post_id = created_account_data_forum["post2"]["id"]
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_reaction/{post_id}", headers=headers)
+
+    assert response.status_code == 403
+    results = response.json()
+    assert results["detail"] == "This post does not belong to your account"
+
+
+def test_delete_reaction_non_existent(authenticate2):
+    post_id = created_account_data_forum["post2"]["id"]
+
+    headers = {
+        "Authorization": f"Bearer {authenticate2}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_reaction/{post_id}", headers=headers)
+
+    assert response.status_code == 400
+    results = response.json()
+    assert results["detail"] == "Reaction does not exists."
+
+
+def test_delete_reaction(authenticate):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_reaction/{post_id}", headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["message"] == "Reaction successfully removed"
+
+
+def test_delete_post_not_found(authenticate):
+    post_id = uuid4()
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_post/{post_id}", headers=headers)
+
+    assert response.status_code == 404
+    results = response.json()
+    assert results["detail"] == "Post forum not found"
+
+
+def test_delete_post_not_belong_user(authenticate):
+    post_id = created_account_data_forum["post2"]["id"]
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_post/{post_id}", headers=headers)
+
+    assert response.status_code == 403
+    results = response.json()
+    assert results["detail"] == "This post forum does not belong to your user"
+
+
+def test_delete_post(authenticate):
+    post_id = created_account_data_forum["post1"]["id"]
+
+    headers = {
+        "Authorization": f"Bearer {authenticate}"
+    }
+
+    response = client.delete(f"/api/v1/forums/delete_post/{post_id}", headers=headers)
+
+    assert response.status_code == 200
+    results = response.json()
+    assert results["message"] == "Post Forum deleted successfully"
+
